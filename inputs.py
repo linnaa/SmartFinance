e45e2eba4fd10d1d863e3e71197452c6e6aad45d
linnaa <linnaa@ukr.net>
2018-06-17 19:00:57 +0300

    inputs module aupdated *with error

--- inputs.py
+++ inputs.py
@@ -1,5 +1,6 @@
 import os
-from constants import *
+from constants import INIT_MENU, INPUT_AMOUNT, INPUT_DATE, ERROR_MSG_WRONG_FORMAT
+import datetime
 
 
 def inputAmount():
@@ -17,5 +18,13 @@ def inputAmount():
 
 
 def inputDate():
-    os.system('cls')
-    return input(INPUT_DATE)
+    date = 0
+    while True:
+        os.system('cls')
+        date = input('input the date in YYYY-MM-DD format: ')
+        try:
+            date = datetime.datetime.strptime(date, '%y-%m-%d')
+            break
+        except ValueError:
+            print("Incorrect data format, should be YYYY-MM-DD")
+            input()
--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 def printData(amount, date):
     print('New bill added:')
     print('amount: {0}'.format(amount))
-    print('date: {0}{1}.{2}{3}.{4}{5}{6}{7}'.format(date[0], date[1], date[3],
-                                                    date[4], date[6], date[7],
-                                                    date[8], date[9]))
+    print('date: ', date)
